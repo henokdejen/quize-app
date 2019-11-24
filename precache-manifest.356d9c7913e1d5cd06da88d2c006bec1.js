8b479ce2ee73a8d1309cd2ec1854b0032e652635
Henok <zaytech2@Henoks-MacBook-Pro.local>
2019-11-24 11:53:07 +0300

    Updates

---
+++ precache-manifest.356d9c7913e1d5cd06da88d2c006bec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a15b959c2d09f50f393693aa4bbb9c1",
+    "url": "/henokdejen/quize-app/index.html"
+  },
+  {
+    "revision": "9733f3d7b591d5318552",
+    "url": "/henokdejen/quize-app/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "7b5734f249bd237d6472",
+    "url": "/henokdejen/quize-app/static/css/main.ccd8118c.chunk.css"
+  },
+  {
+    "revision": "9733f3d7b591d5318552",
+    "url": "/henokdejen/quize-app/static/js/2.2b5ecf04.chunk.js"
+  },
+  {
+    "revision": "7b5734f249bd237d6472",
+    "url": "/henokdejen/quize-app/static/js/main.bd17de45.chunk.js"
+  },
+  {
+    "revision": "bd339a634c37c70d1be7",
+    "url": "/henokdejen/quize-app/static/js/runtime-main.59f832e0.js"
+  }
+]);
